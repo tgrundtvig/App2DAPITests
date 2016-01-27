43c0084ead992e87b2597d665ed0a50e5f53e052
tgrundtvig <tgrundtvig@gmail.com>
2016-01-28 00:36:22 +0100

    Version 0.4

--- src/app2dapitests/App1.java
+++ src/app2dapitests/App1.java
@@ -1,7 +1,8 @@
-package app2dapitests.app1;
+package app2dapitests;
 
 import app2dapi.App2D;
 import app2dapi.Device;
+import app2dapi.Platform;
 import app2dapi.geometry.G2D;
 import app2dapi.geometry.G2D.Polygon;
 import app2dapi.geometry.G2D.PolygonBuilder;
@@ -12,6 +13,7 @@ import app2dapi.input.mouse.MouseMovedEvent;
 import app2dapi.input.mouse.MousePressedEvent;
 import app2dapi.input.mouse.MouseReleasedEvent;
 import app2dapi.input.mouse.MouseWheelEvent;
+import app2dpcimpl.PCPlatformImpl;
 
 /**
  *
@@ -23,6 +25,9 @@ public class App1 implements App2D, MouseListener
     private ColorFactory cf;
     private Polygon arrow;
     private G2D geo;
+    private double startTime;
+    private double test;
+    private double test2;
     
     @Override
     public boolean initialize(Device device)
@@ -36,12 +41,27 @@ public class App1 implements App2D, MouseListener
         p = bld.build();
         arrow = null;//geo.createArrow(geo.newPoint2D(60, 60), geo.newPoint2D(400, 200), 10);
         cf = device.getScreen().getColorFactory();
+        startTime = 0.0;
         return true;
     }
 
     @Override
     public boolean update(double time)
     {
+        double period = 4.0;
+        double periodProgress = periodProgress(time, period);
+        if(periodProgress < 0.5*period)
+        {
+            double f = (periodProgress / period) * 2.0;
+            test = f; //acc(f);
+            test2 = acc(f);
+        }
+        else
+        {
+            double f = ((period - periodProgress) / period) * 2.0;
+            test = f; //acc(f);
+            test2 = acc(f);
+        }
         return true;
     }
 
@@ -52,6 +72,19 @@ public class App1 implements App2D, MouseListener
         //canvas.setTransformation(t);
         canvas.clear(cf.getWhite());
         canvas.setColor(cf.getBlack());
+        
+        //Linear motion test
+        canvas.drawPoint(geo.newPoint2D((float)(test*200+100), 100), 10);
+        canvas.drawPoint(geo.newPoint2D(100, 120), 5);
+        canvas.drawPoint(geo.newPoint2D(200, 120), 5);
+        canvas.drawPoint(geo.newPoint2D(300, 120), 5);
+        
+        //Constant acceleration motion test
+        canvas.drawPoint(geo.newPoint2D((float)(test2*200+100), 150), 10);
+        canvas.drawPoint(geo.newPoint2D(100, 170), 5);
+        canvas.drawPoint(geo.newPoint2D(200, 170), 5);
+        canvas.drawPoint(geo.newPoint2D(300, 170), 5);
+        
         canvas.drawFilledPolygon(p);
         if(arrow != null)
         {
@@ -96,6 +129,33 @@ public class App1 implements App2D, MouseListener
     public boolean showMouseCursor()
     {
         return false;
+    }
+    
+    private static double acc(double t)
+    {
+        if(t < 0.5)
+        {
+            return 2.0 * t * t;
+        }
+        t = t - 0.5;
+        return 0.5 + 2 * t *( 1 - t );
+    }
+    
+    private static double periodProgress(double time, double period)
+    {
+        double progress = time / period;
+        double holePeriods =  Math.floor(progress);
+        return time - holePeriods * period;
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        Platform p = new PCPlatformImpl();
+        App2D app = new App1();
+        p.runApplication(app);
     }
     
 }
